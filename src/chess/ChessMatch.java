8335f921a1f8c1b62cb4361cba760ad1f4fe051e
Ana Sousa <anadeso90@gmail.com>
2020-06-23 14:06:29 -0300

    Little improvement in board printing

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -35,8 +35,18 @@ public class ChessMatch {
     }
     // Iniciando a partida de xadrez, colocando as pecas no tabuleiro
     private void initialSetup(){
-        placeNewPiece('b', 6, new Rock(board,Color.WHITE));
-        placeNewPiece('e', 8 ,new King(board,Color.BLACK));
-        placeNewPiece('e', 1,new King(board,Color.WHITE));
+        placeNewPiece('c', 1, new Rock(board, Color.WHITE));
+        placeNewPiece('c', 2, new Rock(board, Color.WHITE));
+        placeNewPiece('d', 2, new Rock(board, Color.WHITE));
+        placeNewPiece('e', 2, new Rock(board, Color.WHITE));
+        placeNewPiece('e', 1, new Rock(board, Color.WHITE));
+        placeNewPiece('d', 1, new King(board, Color.WHITE));
+
+        placeNewPiece('c', 7, new Rock(board, Color.BLACK));
+        placeNewPiece('c', 8, new Rock(board, Color.BLACK));
+        placeNewPiece('d', 7, new Rock(board, Color.BLACK));
+        placeNewPiece('e', 7, new Rock(board, Color.BLACK));
+        placeNewPiece('e', 8, new Rock(board, Color.BLACK));
+        placeNewPiece('d', 8, new King(board, Color.BLACK));
     }
 }
