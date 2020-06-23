5a91da7d4394afa1a8fabebad3a6cfbe6979cea3
Ana Sousa <anadeso90@gmail.com>
2020-06-23 17:07:39 -0300

    Possible moves of a piece

--- src/boardgame/Piece.java
+++ src/boardgame/Piece.java
@@ -1,6 +1,6 @@
 package boardgame;
 
-public class Piece {
+public abstract class Piece {
 
     // Classe Peca tem posicao protegida, na qual nao é a posicao do xadrez, mas
     // sim um array simples
@@ -18,4 +18,26 @@ public class Piece {
     protected Board getBoard() {
         return board;
     }
+
+    // Nao seu a possicao por isso declarei o metodo como abstrato
+    public abstract  boolean [][] possibleMoves();
+
+    // Metodo concreto no qual esta dependendo do metodo abstrato
+    public boolean possibleMove(Position position){
+        return possibleMoves()[position.getRow()][position.getColumn()];
+    }
+
+    public boolean isThereAnyPossibleMove(){
+        boolean[][] mat = possibleMoves();
+        for (int i=0; i<mat.length; i++){
+            for (int j=0; j<mat.length; j++){
+                if (mat[i][j]){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+
 }
--- src/chess/ChessPiece.java
+++ src/chess/ChessPiece.java
@@ -3,7 +3,7 @@ package chess;
 import boardgame.Board;
 import boardgame.Piece;
 
-public class ChessPiece extends Piece {
+public abstract class ChessPiece extends Piece {
 
     // Classe ChessPiece(Peca do xadrex)
     private Color color;
